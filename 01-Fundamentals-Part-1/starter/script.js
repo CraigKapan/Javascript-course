d4cfaa9e8c4c38c077b496fa0b75e90f04277bc3
unknown <craigkapan@gmail.com>
2023-04-05 12:23:24 +0200

    10. Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,25 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is fun!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Craig");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let craig_matilda = "CM";
+let $function = 27;
+
+let person = "craig";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let currentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
