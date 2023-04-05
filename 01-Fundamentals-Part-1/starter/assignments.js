14c2777716e3872e123f0fb580ac47669e0f31ca
unknown <craigkapan@gmail.com>
2023-04-05 12:57:53 +0200

    13. let, const and var, and assingment on it

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -17,7 +17,11 @@ console.log(population);
 let isIsland = false;
 let language;
 
+/*
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+*/
+
+language = "english";
