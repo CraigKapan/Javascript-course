ed0fb18ead18603095baa9a6e82e5c83ded4379c
unknown <craigkapan@gmail.com>
2023-04-05 12:46:40 +0200

    12. Data Types, and assignment about it

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,23 @@
+/* ************************** 
+Values and Variables
+************************** */
+
+
+let country = "South Africa";
+let continent = "Africa";
+let population = 59.39;
+
+/*
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
