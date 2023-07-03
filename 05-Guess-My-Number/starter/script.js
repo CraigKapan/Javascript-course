d6634cdc55b0570f5bba1876a43f7fa8d3a93653
unknown <craigkapan@gmail.com>
2023-07-03 14:02:46 +0200

    75. Manipulating CSS Styles

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -28,6 +28,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').innerHTML = '⛔ No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').innerHTML = '🎉 Correct Number!';
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').innerHTML = '📈 Too High!';
