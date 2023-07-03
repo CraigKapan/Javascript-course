a8d2a74b04eba66a3adfe6511fb877115493165b
unknown <craigkapan@gmail.com>
2023-07-03 13:52:55 +0200

    74. Implementing the Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,13 +14,37 @@ document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
 */
 /////////////////////////////
-// Handling Click Events
+// Project: Guess my number
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').innerHTML = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector(".message").innerHTML = "⛔ No number!";
+    document.querySelector('.message').innerHTML = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').innerHTML = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').innerHTML = '📈 Too High!';
+      score--;
+      document.querySelector('.score').innerHTML = score;
+    } else {
+      document.querySelector('.message').innerHTML = '💥 You lost the game!';
+      document.querySelector('.score').innerHTML = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').innerHTML = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').innerHTML = score;
+    } else {
+      document.querySelector('.message').innerHTML = '💥 You lost the game!';
+      document.querySelector('.score').innerHTML = 0;
+    }
   }
 });
