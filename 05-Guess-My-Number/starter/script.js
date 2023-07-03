09f89265c5eeb2af4292e8ad08b5e0105a42ee82
unknown <craigkapan@gmail.com>
2023-07-03 13:14:49 +0200

    72. Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,13 @@
 'use strict';
 
 /////////////////////////////
-// PROJECT #1: Guess My Number!
+// Selecting and Manipulating Elements
 
 console.log(document.querySelector(".message").innerHTML);
+document.querySelector(".message").innerHTML = "🎉 Correct Number!"
+
+document.querySelector(".number").innerHTML = 13;
+document.querySelector(".score").innerHTML = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
