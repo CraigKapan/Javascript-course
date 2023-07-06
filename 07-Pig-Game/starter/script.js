cd196ce177c76a20363c70efe8bf4ad9752165ff
unknown <craigkapan@gmail.com>
2023-07-06 12:54:38 +0200

    82. PROJECT #3: Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+////////////////////////////////////
+// PROJECT #3: Pig Game
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector(".dice");
+
+// Starting conditions
+score0El.innerHTML = 0;
+score1El.innerHTML = 0;
+diceEl.classList.add("hidden");
